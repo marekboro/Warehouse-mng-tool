9addb97a818d31a5022a9d852220843e8e9e2904
marekboro <marek.borowicz83@gmail.com>
2020-08-11 23:15:20 +0100

    adding editing and deleting of products, brands and types possible via the interface

--- controllers/product_types_controller.py
+++ controllers/product_types_controller.py
@@ -9,7 +9,6 @@ from models.stock import Stock
 import repositories.brand_repository as brand_repository
 import repositories.stock_repository as stock_repository
 
-
 import repositories.product_type_repository as product_type_repository
 import repositories.product_repository as product_repository
 
@@ -27,3 +26,29 @@ def types_editing_view():
 def delete_product_type(id):
     product_type_repository.delete(id)
     return redirect("/edit/type-edit")
+
+@product_types_blueprint.route("/edit/modifyType<id>", methods = ['post'])
+def update_product_type(id):
+    modified_product_type = ProductType(request.form['newTypeName'],id)
+
+    product_type_repository.update(modified_product_type)
+    
+    return redirect("/edit/type-edit")
+
+@product_types_blueprint.route("/edit/createType", methods = ['post'])
+def add_product_type():
+    new_type = ProductType(request.form['newTypeName'])
+    
+    product_type_repository.save(new_type)
+    return redirect("/edit/type-edit")
+    # types = product_type_repository.select_all()
+    # return render_template("/edit/type-edit", types = types)
+
+
+# @brands_blueprint.route("/edit/createNewBrand", methods = ['post'])  # create a brand from the form details
+# def adding_new_brand():
+#     new_brand = Brand(request.form['newBrandName'],request.form['newBrandDescription'],request.form['newBrandWarranty'])
+#     brand_repository.save(new_brand)
+#     return redirect("/edit/brand-edit")
+
+
